671e0ff84950a4015768eddbfb9ec4245f36455a
cfarot <charlyfarot@gmail.com>
2015-06-23 15:37:45 +0200

    generate key Cesar

--- src/Cesar.java
+++ src/Cesar.java
@@ -1,12 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class Cesar implements ICipher {
 
 	@Override
 	public File encode(File message, File key, File encoded) {
-		// TODO Auto-generated method stub
+		
+		String keyStr = "";
+		BufferedReader br = null;
+		
+		try {
+
+			br = new BufferedReader(new FileReader(key));
+			keyStr = br.readLine();
+			br.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		
 		return null;
+		//for 
+		
 	}
 
 	@Override
@@ -17,7 +37,22 @@ public class Cesar implements ICipher {
 
 	@Override
 	public void generateKey(File myKey) {
-		// TODO Auto-generated method stub
+		
+		String myKeyStr;
+		
+		String keysPossible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int randomNum = (int) (Math.random() * keysPossible.length());
+		
+		myKeyStr = String.valueOf(keysPossible.charAt(randomNum));
+		FileWriter fw;
+		try {
+			fw = new FileWriter(myKey.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(myKeyStr);
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
